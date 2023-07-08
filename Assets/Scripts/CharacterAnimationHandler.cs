a156b7a4f310e3dd05be0b6dfecd3d8ca4ae2b8b
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-07-08 11:21:16 -0400

    adding random animations

--- Assets/Scripts/CharacterAnimationHandler.cs
+++ Assets/Scripts/CharacterAnimationHandler.cs
@@ -10,10 +10,32 @@ public class CharacterAnimationHandler : MonoBehaviour
 
     public enum States
     {
-        Idle, GetHit, Taunt, Defeated, Death, Twerk
+        Idle, GetHit, Taunt, Defeated, Twerk,
+
+        Max, 
+        
+        Death
     }
 
     [SerializeField] States targetState; // use this to set states
+
+    private void Start()
+    {
+        StartCoroutine(PlayAnimation());
+    }
+
+    // runs to play animation
+    IEnumerator PlayAnimation()
+    {
+        // choose a random animation
+        RequestAnimation((States)Random.Range(0, (int)States.Max));
+
+        // wait random time
+        yield return new WaitForSecondsRealtime(Random.Range(3f, 6f));
+
+        // do it again
+        StartCoroutine(PlayAnimation());
+    }
 
     private void FixedUpdate()
     {
