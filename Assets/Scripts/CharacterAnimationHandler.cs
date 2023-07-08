dd82a2bd7251f8f9ee3db1aa00b2bbd89ab6f20d
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-07-08 10:45:58 -0400

    Added player character animation controller and player character rig

---
+++ Assets/Scripts/CharacterAnimationHandler.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationHandler : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+
+    [SerializeField] float moveSpeed;
+
+    public enum States
+    {
+        Idle, GetHit, Taunt, Defeated, Death
+    }
+
+    [SerializeField] States targetState; // use this to set states
+
+    private void FixedUpdate()
+    {
+        if (targetState != States.Idle)
+            RequestAnimation(targetState);
+
+        LerpBackToRoot();
+    }
+
+    public void RequestAnimation(States state)
+    {
+        // play our animation
+        animator.Play(state.ToString());
+        // reset the state
+        targetState = States.Idle;
+    }
+
+    // lerp back to start if state is idle
+    public void LerpBackToRoot()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle") || animator.GetCurrentAnimatorStateInfo(0).IsName("Idle 1"))
+        {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, Vector3.zero, moveSpeed * Time.deltaTime);
+        }
+    }
+}
